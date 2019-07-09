67321bde0cd98084879342cd36cdbf09bc93583f
Tobey Yang <myang1@godaddy.com>
2019-07-08 21:49:21 -0700

    6.9

--- epi_judge_java/epi/ApplyPermutation.java
+++ epi_judge_java/epi/ApplyPermutation.java
@@ -1,11 +1,20 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+import java.util.Collections;
 import java.util.List;
 public class ApplyPermutation {
   public static void applyPermutation(List<Integer> perm, List<Integer> A) {
     // TODO - you fill in here.
-    return;
+    for(int i = 0; i < perm.size(); i++){
+      int cur = i;
+      while(perm.get(next) >= 0){
+        int next = perm.get(next);
+        Collections.swap(A, i, next);
+        perm.set(cur, perm.get(cur) - perm.size());
+        cur = next;
+      }
+    }
   }
   @EpiTest(testDataFile = "apply_permutation.tsv")
   public static List<Integer> applyPermutationWrapper(List<Integer> perm,
