31072494f805beb0299311369e46e50ef7247508
Tobey Yang <myang1@godaddy.com>
2019-07-08 21:47:27 -0700

    6.4

--- epi_judge_java/epi/AdvanceByOffsets.java
+++ epi_judge_java/epi/AdvanceByOffsets.java
@@ -6,7 +6,12 @@ public class AdvanceByOffsets {
   @EpiTest(testDataFile = "advance_by_offsets.tsv")
   public static boolean canReachEnd(List<Integer> maxAdvanceSteps) {
     // TODO - you fill in here.
-    return true;
+    int furthest = 0;
+    int lastIndex = maxAdvanceSteps.size() - 1;
+    for(int i = 0; i <= furthest && i < lastIndex; i++){
+      furthest = Math.max(furthest, i + maxAdvanceSteps.get(i));
+    }
+    return furthest >= lastIndex;
   }
 
   public static void main(String[] args) {
