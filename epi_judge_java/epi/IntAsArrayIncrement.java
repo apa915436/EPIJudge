2a6d2e5a3bf3aedc3f834c3bd78e6164a28a091f
Tobey Yang <myang1@godaddy.com>
2019-07-08 21:46:26 -0700

    6.2

--- epi_judge_java/epi/IntAsArrayIncrement.java
+++ epi_judge_java/epi/IntAsArrayIncrement.java
@@ -6,7 +6,19 @@ public class IntAsArrayIncrement {
   @EpiTest(testDataFile = "int_as_array_increment.tsv")
   public static List<Integer> plusOne(List<Integer> A) {
     // TODO - you fill in here.
-    return null;
+    for(int i = A.size() - 1; i >= 0; i--){
+      if (A.get(i) == 9){
+        A.set(i, 0);
+      }else{
+        A.set(i, A.get(i) + 1);
+        break;
+      }
+      if(i == 0){
+        A.set(0, 1);
+        A.add(0);
+      }
+    }
+    return A;
   }
 
   public static void main(String[] args) {
